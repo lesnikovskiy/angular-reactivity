bb4b42dca90367f9a90116520d7b8abadcafadc9
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2024-01-22 11:23:38 +0200

    Fix Unidirectional flow caused expression changed error

--- src/app/directives/illegal.directive.ts
+++ src/app/directives/illegal.directive.ts
@@ -8,11 +8,14 @@ import { ModelDirective } from "./model.directive";
 export class IllegalDirective {
   private readonly modelDirective = inject(ModelDirective, { self: true });
 
-  @Input({ required: true }) illegal!: string;
+  private _illegal = '';
+  @Input({ required: true }) set illegal(value: string) {
+    setTimeout(() => this._illegal = value, 0);
+  }
 
   ngOnInit(): void {
     this.modelDirective.model.addValidator(value => {
-      for (const ch of this.illegal) {
+      for (const ch of this._illegal) {
         if (value.includes(ch)) {
           return false;
         }
